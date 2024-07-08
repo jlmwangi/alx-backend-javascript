b4c7c016bf452da82bdf8b0627e8338ed1301db9
mwangii <jlmwangi254@gmail.com>
2024-07-08 10:32:31 +0300

    promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      const { body } = photoResponse;
+    .then(([{ body }, userResponse]) => {
       const { firstName, lastName } = userResponse;
       console.log(`${body} ${firstName} ${lastName}`);
     })
