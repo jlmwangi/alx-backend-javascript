8f07fdf1e674c56aed4338f373cc20af5ebd6340
mwangii <jlmwangi254@gmail.com>
2024-07-11 11:53:55 +0300

    try to debug

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,5 +1,5 @@
-//const class2019 = new HolbertonClass(2019, 'San Francisco');
-//const class2020 = new HolbertonClass(2020, 'San Francisco');
+// const class2019 = new HolbertonClass(2019, 'San Francisco');
+// const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class HolbertonClass {
   constructor(year, location) {
@@ -16,14 +16,14 @@ export class HolbertonClass {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
+//const class2019 = new HolbertonClass(2019, 'San Francisco');
+//const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-//const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-//const student2 = new StudentHolberton('John', 'Doe', class2020);
-//const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-//const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-//const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+// const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+// const student2 = new StudentHolberton('John', 'Doe', class2020);
+// const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+// const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+// const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
@@ -44,13 +44,17 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
+const createInstances = () => {
+  const class2019 = new HolbertonClass(2019, 'San Francisco');
+  const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+  const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+  const student2 = new StudentHolberton('John', 'Doe', class2020);
+  const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+  const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+  const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
+  return [student1, student2, student3, student4, student5];
+};
 
-
-export const listOfStudents = [student1, student2, student3, student4, student5];
+export const listOfStudents = createInstances();
