b76e999d004da52e0d90dfc94d3438554b3e1562
mwangii <jlmwangi254@gmail.com>
2024-07-11 10:05:34 +0300

    to string

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name && typeof code !== 'string') {
+      throw new TypeError('values must be strings');
+    }
+    this._name = name;
+    this._code = code;
+  }
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
