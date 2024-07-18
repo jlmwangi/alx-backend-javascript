9132b4b33ba6ef1f469350ecdd67d453ad845c7d
mwangii <jlmwangi254@gmail.com>
2024-07-18 15:54:52 +0300

    combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  // new grades is an array of objects
+  const filteredStudents = students.filter((student) => student.location === city);
+  const res = filteredStudents.map((student) => {
+    const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+    const newGrade = gradeObject ? gradeObject.grade : 'N/A';
+    return {
+	    id: student.id, firstName: student.firstName, location: city, grade: newGrade,
+    };
+  });
+  return res;
+}
