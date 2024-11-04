1ebaa9e377c41ca880131dc21f4f781553c563eb
mwangii <jlmwangi254@gmail.com>
2024-11-04 19:07:50 +0300

    commit errors

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,35 +1,37 @@
 const fs = require('node:fs/promises');
 
 function countStudents(path) {
-  return fs.readFile(path, { encoding: 'utf8' })
-    .then((data) => {
-      const lines = data.trim().split('\n');
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf8' })
+      .then((data) => {
+        const lines = data.trim().split('\n');
 
-      let countCs = 0;
-      let countSwe = 0;
-      const csStudents = [];
-      const sweStudents = [];
+        let countCs = 0;
+        let countSwe = 0;
+        const csStudents = [];
+        const sweStudents = [];
 
-      for (let i = 1; i < lines.length; i += 1) {
-        const [firstName, , , field] = lines[i].split(',');
+        for (let i = 1; i < lines.length; i += 1) {
+          const [firstName, , , field] = lines[i].split(',');
 
-        if (field === 'CS') {
-          countCs += 1;
-          csStudents.push(firstName);
-        } else if (field === 'SWE') {
-          countSwe += 1;
-          sweStudents.push(firstName);
+          if (field === 'CS') {
+            countCs += 1;
+            csStudents.push(firstName);
+          } else if (field === 'SWE') {
+            countSwe += 1;
+            sweStudents.push(firstName);
+          }
         }
-      }
 
-      console.log(`Number of students: ${countCs + countSwe}`);
+        console.log(`Number of students: ${countCs + countSwe}`);
+        console.log(`Number of students in CS: ${countCs}. List: ${csStudents.join(', ')}`);
+        console.log(`Number of students in SWE: ${countSwe}. List: ${sweStudents.join(', ')}`);
 
-      console.log(`Number of students in CS: ${countCs}. List: ${csStudents.join(', ')}`);
-      console.log(`Number of students in SWE: ${countSwe}. List: ${sweStudents.join(', ')}`);
-    })
-    .catch((error) => {
-    // console.error(`Error loading: ${error.message}`);
-      throw new Error('Cannot load the database');
+        resolve();
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
   });
 }
 
