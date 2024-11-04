3393a1b2e05eaffbc9c442c14b5fff91d68217a5
mwangii <jlmwangi254@gmail.com>
2024-11-04 10:19:12 +0300

    try

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,15 +4,15 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const name = data.trim()
+  const name = data.trim();
   console.log(`Your name is: ${name}`);
   process.stdin.pause();
 });
 
-//process.on('SIGINT', () => {
-  //console.log('This important software is now closing');
-  //process.exit();
-//});
+process.on('SIGINT', () => {
+  console.log('This important software is now closing');
+  process.exit();
+});
 
 process.on('exit', () => {
   console.log('This important software is now closing');
