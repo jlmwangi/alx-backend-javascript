35d0f7b896cbc8dfae54751c157ac531d0626fbf
mwangii <jlmwangi254@gmail.com>
2024-11-04 17:41:14 +0300

    use async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('node:fs/promises');
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, { encoding: 'utf8' });
+
+    const lines = data.trim().split('\n');
+
+    let countCs = 0;
+    let countSwe = 0;
+    const csStudents = [];
+    const sweStudents = [];
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const [firstName, , , field] = lines[i].split(',');
+
+      if (field === 'CS') {
+        countCs += 1;
+        csStudents.push(firstName);
+      } else if (field === 'SWE') {
+        countSwe += 1;
+        sweStudents.push(firstName);
+      }
+    }
+
+    console.log(`Number of students: ${countCs + countSwe}`);
+
+    console.log(`Number of students in CS: ${countCs}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${countSwe}. List: ${sweStudents.join(', ')}`);
+  } catch (error) {
+    // console.error(`Error loading: ${error.message}`);
+    throw new Error('Cannot load the database');
+  }
+}
+
+// countStudents('path/to/file');
+
+module.exports = countStudents;
