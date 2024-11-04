9397c00ede7d125cba3c1aee7139e95b44d5bd3d
mwangii <jlmwangi254@gmail.com>
2024-11-04 19:28:05 +0300

    create http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const app = require('http');
+
+app.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello Holberton School!');
+  res.end();
+}).listen(1245);
+
+module.exports = app;
